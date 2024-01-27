1403cdd7fd679a15ade2db3dd1a1b1a9ec9e6687
metzoo10 <mister_mef365@outlook.com>
2024-01-27 17:15:56 +0000

    implement of swiper.js | add image slider to the homepage | searchbar replaced | new images added | dark mode toggle button added in settings page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -14,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
+  darkMode = false;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.checkAppMode();
   }
 
+  checkAppMode() {
+    const checkIsDarkMode = localStorage.getItem('darkModeActivated');
+    checkIsDarkMode == 'true'
+      ? (this.darkMode = true)
+      : (this.darkMode = false);
+    document.body.classList.toggle('dark', this.darkMode); 
+  }
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark', this.darkMode);
+    if(this.darkMode) {
+      localStorage.setItem('darkModeActivated', 'true');
+    } else {
+      localStorage.setItem('darkModeActivated', 'false');
+    }
+  }
 }
